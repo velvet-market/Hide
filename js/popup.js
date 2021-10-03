2b727d9962aa089945ae59254be5313b9afab654
marla0530 <haotianzheng3@gmail.com>
2021-10-03 02:42:53 -0400

    added jquery

--- js/popup.js
+++ js/popup.js
@@ -1,17 +1,14 @@
-document.addEventListener("DOMContentLoaded", documentEvents, false);
-
-const callAlert = (input) => {
-  alert("The entered url is : " + input.value);
-}
-
-function documentEvents() {
-  let button = document.getElementById("confirm_btn")
-  button.addEventListener("click", () => {
-    let urlBox = document.getElementById("url_textbox");
-    callAlert(urlBox);
-
+$(document).ready(() => {  
+  $("#confirm_btn").on("click", () => {
+    let urlBox = $("#url_textbox")
+    // callAlert(urlBox);
+    
     chrome.storage.sync.set({
-      "url": urlBox.value
+      "url": urlBox.val()
     })
   })
+})
+
+const callAlert = (input) => {
+  alert("The entered url is : " + input.val());
 }
